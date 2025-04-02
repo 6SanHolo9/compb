3c9352eef9dfd1cf05cbcca1002d022b1c2a4ae8
SanHolo <kartikdangi7@gmail.com>
2025-04-03 00:43:47 +0530

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Create a directory named dir2
+# Create a directory named dir2 (if it doesn't already exist)
 mkdir -p dir2
 
-# Move all text files to dir2
+# Move all .txt files from the current directory to dir2
 mv *.txt dir2/
 
-# Stage the moved files
-git add dir2/*.txt
+# Stage all changes (both the removal and addition of files)
+git add -A
 
-# Commit the changes
+# Commit the changes with a message
 git commit -m "Move all text files to dir2"
